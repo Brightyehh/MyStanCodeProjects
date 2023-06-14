d520a7c0d66eb79537b6a4d903be9d41d7b40c5f
Brightyehh <136299143+Brightyehh@users.noreply.github.com>
2023-06-14 11:53:23 +0800

    Add files via upload

--- stanCode_Projects/05_break_out_game/breakout.py
+++ stanCode_Projects/05_break_out_game/breakout.py
@@ -13,37 +13,45 @@ import math
 
 
 FRAME_RATE = 10         # 100 frames per second, initial 10
-NUM_LIVES = 3			# Number of attempts, initial3
 
 
 def main():
-    graphics = BreakoutGraphics()
+    graphics = BreakoutGraphics(brick_rows=10, brick_cols=10, paddle_width=150)
     graphics.set_ball_velocity()
     dx = graphics.get_dx()
     dy = graphics.get_dy()
-    lives = NUM_LIVES
+
+    # for check
+    c1 = graphics.collision_1()
+    c2 = graphics.collision_2()
+    c3 = graphics.collision_3()
+    c4 = graphics.collision_4()
 
     # Add the animation loop here!
     while True:
         pause(FRAME_RATE)
-        brick_num = graphics.brick_num
-        remove_num = graphics.remove_num
         # Check if clear
-        if brick_num == remove_num:
+        if graphics.clear():
             break
         # Check the lives
         if graphics.ball_out_of_window():
-            lives -= 1
             graphics.reset_ball()
             graphics.set_ball_velocity()
             dx = graphics.get_dx()
             dy = graphics.get_dy()
-            if lives == 0:
+            graphics.live_update()
+            if graphics.fail():
                 break
+        # speed up!!!
+        if graphics.remove_num >= graphics.brick_num/2:
+            if graphics.start:
+                if graphics.moving:
+                    graphics.ball.move(dx*1.5, dy*1.5)
         # Check the condition and moving the ball, collision with window
-        if graphics.start:
-            if graphics.moving:
-                graphics.ball.move(dx, dy)
+        else:
+            if graphics.start:
+                if graphics.moving:
+                    graphics.ball.move(dx, dy)
         if graphics.ball.x <= 0 or graphics.ball.x+graphics.ball.width >= graphics.window.width:
             dx = -dx
         if graphics.ball.y <= 0:
@@ -58,6 +66,7 @@ def main():
             dx = math.fabs(dx)
         if graphics.collision_4():
             dx = -math.fabs(dx)
+        graphics.score_update()
 
 
 if __name__ == '__main__':
